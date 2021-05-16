abada323371eb4f7147f61e845c3cbd914c5d89e
jaibymol4@gmail.com <jaibymol4@gmail.com>
2021-05-16 22:56:58 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPatientName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.rdbtnChainsmokerYes = new System.Windows.Forms.RadioButton();
+            this.txtMedicineDosage = new System.Windows.Forms.TextBox();
+            this.rdbtnChainsmokerNo = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPatientName
+            // 
+            this.txtPatientName.Location = new System.Drawing.Point(158, 96);
+            this.txtPatientName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPatientName.Name = "txtPatientName";
+            this.txtPatientName.Size = new System.Drawing.Size(132, 22);
+            this.txtPatientName.TabIndex = 0;
+            this.txtPatientName.TextChanged += new System.EventHandler(this.txtPatientName_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(158, 180);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(132, 22);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(158, 135);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(132, 22);
+            this.txtAge.TabIndex = 2;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // rdbtnChainsmokerYes
+            // 
+            this.rdbtnChainsmokerYes.AutoSize = true;
+            this.rdbtnChainsmokerYes.Location = new System.Drawing.Point(158, 230);
+            this.rdbtnChainsmokerYes.Margin = new System.Windows.Forms.Padding(4);
+            this.rdbtnChainsmokerYes.Name = "rdbtnChainsmokerYes";
+            this.rdbtnChainsmokerYes.Size = new System.Drawing.Size(53, 21);
+            this.rdbtnChainsmokerYes.TabIndex = 3;
+            this.rdbtnChainsmokerYes.TabStop = true;
+            this.rdbtnChainsmokerYes.Text = "Yes";
+            this.rdbtnChainsmokerYes.UseVisualStyleBackColor = true;
+            // 
+            // txtMedicineDosage
+            // 
+            this.txtMedicineDosage.Location = new System.Drawing.Point(158, 271);
+            this.txtMedicineDosage.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMedicineDosage.Name = "txtMedicineDosage";
+            this.txtMedicineDosage.Size = new System.Drawing.Size(132, 22);
+            this.txtMedicineDosage.TabIndex = 4;
+            this.txtMedicineDosage.TextChanged += new System.EventHandler(this.txtMedicineDosage_TextChanged);
+            // 
+            // rdbtnChainsmokerNo
+            // 
+            this.rdbtnChainsmokerNo.AutoSize = true;
+            this.rdbtnChainsmokerNo.Location = new System.Drawing.Point(223, 230);
+            this.rdbtnChainsmokerNo.Margin = new System.Windows.Forms.Padding(4);
+            this.rdbtnChainsmokerNo.Name = "rdbtnChainsmokerNo";
+            this.rdbtnChainsmokerNo.Size = new System.Drawing.Size(47, 21);
+            this.rdbtnChainsmokerNo.TabIndex = 5;
+            this.rdbtnChainsmokerNo.TabStop = true;
+            this.rdbtnChainsmokerNo.Text = "No";
+            this.rdbtnChainsmokerNo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Patient Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 230);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Is Chainsmoker";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 183);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 274);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Medicine Dosage";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAdd.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(100, 325);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 45);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.rdbtnChainsmokerYes);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtPatientName);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtAge);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtMedicineDosage);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.rdbtnChainsmokerNo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(331, 393);
+            this.panel1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label6.Font = new System.Drawing.Font("Charlemagne Std", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(71, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(217, 33);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Co-Vaccine";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 396);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPatientName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.RadioButton rdbtnChainsmokerYes;
+        private System.Windows.Forms.TextBox txtMedicineDosage;
+        private System.Windows.Forms.RadioButton rdbtnChainsmokerNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAge.Text == "")
+            {
+                MessageBox.Show("Please Enter Age");
+            }
+            else if (!Regex.Match(txtAge.Text, "^[0-9]*$").Success)
+            {
+                MessageBox.Show("Enter a Valid Number");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPatientName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPatientName.Text == "")
+            {
+                MessageBox.Show("Please Enter Name");
+            }
+            else if (Regex.Match(txtPatientName.Text, "^[a-zA-Z ]*$").Success)
+            {
+                MessageBox.Show("Enter a Valid Name");
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                MessageBox.Show("Please Enter Email");
+            }
+            else if (!Regex.Match(txtEmail.Text, "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+$").Success)
+            {
+                MessageBox.Show("Enter a Valid Email");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtPatientName.Text == "" && txtEmail.Text == "" && txtAge.Text == "" && txtMedicineDosage.Text == "")
+            {
+                MessageBox.Show("Enter All The Fields");
+            }
+            else if (txtEmail.Text == "" && txtAge.Text == "" && txtMedicineDosage.Text == "")
+            {
+                MessageBox.Show("Please Enter Age , Email & Medicine");
+            }
+            else if (txtEmail.Text == "")
+            {
+                MessageBox.Show("Please Enter Email");
+            }
+            else if (txtAge.Text == "" && txtMedicineDosage.Text == "")
+            {
+                MessageBox.Show("Please Enter Age & Medicine");
+            }
+            else if (txtMedicineDosage.Text == "")
+            {
+                MessageBox.Show("Please Enter Medicine Dosage");
+            }
+            else if (txtAge.Text == "")
+            {
+                MessageBox.Show("Please Enter Age");
+            }
+            //else if (rdbtnChainsmokerNo.Checked == true || rdbtnChainsmokerYes.Checked == true)
+            //{
+            //    MessageBox.Show("Are you a chainsmoker? please select");
+            //}
+            else if (Convert.ToInt32(txtAge.Text) >= 4 && Convert.ToInt32(txtAge.Text) <= 18 && rdbtnChainsmokerYes.Checked == true)
+            {
+                if (Convert.ToInt32(txtMedicineDosage.Text) <= 2.5 && Convert.ToInt32(txtMedicineDosage.Text) >= 2)
+                {
+                    MessageBox.Show("Approved");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage! 2-2.5 doses needed");
+                }
+            }
+            else if (rdbtnChainsmokerYes.Checked == true && Convert.ToInt32(txtAge.Text) >= 19 && Convert.ToInt32(txtAge.Text) <= 55)
+            {
+                if (Convert.ToDecimal(txtMedicineDosage.Text) >= 1 && Convert.ToDecimal(txtMedicineDosage.Text) <= 2)
+                {
+                    MessageBox.Show("Approved");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage!  1-2 doses needed");
+                }
+            }
+            else if (rdbtnChainsmokerYes.Checked == false && Convert.ToInt32(txtAge.Text) >= 4 && Convert.ToInt32(txtAge.Text) <= 18)
+            {
+                if (Convert.ToDecimal(txtMedicineDosage.Text) >= 1 && Convert.ToDouble(txtMedicineDosage.Text) <= 2.2)
+                {
+                    MessageBox.Show("Correct Medicine You Opted");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage!  1-2.2 doses needed");
+                }
+            }
+            else if (rdbtnChainsmokerYes.Checked == false && Convert.ToInt32(txtAge.Text) >= 19 && Convert.ToInt32(txtAge.Text) <= 30)
+            {
+                if (Convert.ToDouble(txtMedicineDosage.Text) >= 2.3 && Convert.ToDouble(txtMedicineDosage.Text) <= 4.4)
+                {
+                    MessageBox.Show("Correct Medicine You Opted");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage!  2.3-4.4 doses needed");
+                }
+            }
+            else if (rdbtnChainsmokerYes.Checked == false && Convert.ToInt32(txtAge.Text) >= 31 && Convert.ToInt32(txtAge.Text) <= 55)
+            {
+                if (Convert.ToDouble(txtMedicineDosage.Text) >= 4.4 && Convert.ToDecimal(txtMedicineDosage.Text) <= 6)
+                {
+                    MessageBox.Show("Correct Medicine You Opted");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage!  4.4-6 doses needed");
+                }
+            }
+            else if (rdbtnChainsmokerYes.Checked == false && Convert.ToInt32(txtAge.Text) >= 55)
+            {
+                if (Convert.ToDouble(txtMedicineDosage.Text) >= 3.3 && Convert.ToDouble(txtMedicineDosage.Text) <= 4.5)
+                {
+                    MessageBox.Show("Correct Medicine You Opted");
+                }
+                else
+                {
+                    MessageBox.Show("wrong medicine dosage!  3.3-4.5 doses needed");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Successfully Added");
+            }
+
+
+        }
+
+        private void txtMedicineDosage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
+
